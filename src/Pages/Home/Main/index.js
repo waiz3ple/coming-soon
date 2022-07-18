1639afe5a46fc92d4b886f9e4fa460a759e62c80
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2022-07-18 14:53:09 -0400

    working on the glass footer

--- src/Pages/Home/Main/index.js
+++ src/Pages/Home/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import Circle from './Circle';
+import Footer from  '../Footer';
 import './styles.scss';
 
 
@@ -38,6 +39,10 @@ export default class Main extends Component {
           <Circle className="circle1" info={{size:14, colorStart:"#7F1E7B", colorStop:"#19073B"}}/>
           <Circle className="circle2" info={{size:18.4, colorStart:"#7F4129", colorStop:"#1C0C28"}}/>
           <Circle className="circle3" info={{size:10.4, colorStart:"#213F72", colorStop:"#19073B"}}/>
+        </section>
+
+        <section className="footer-wrapper">
+           <Footer/>
         </section>
       </>
     );
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles    from './styles.scss';
 import Header    from './Header';
 import Main      from './Main';
-
+import Footer    from  './Footer';
 
 class Home extends Component {
   render() {
